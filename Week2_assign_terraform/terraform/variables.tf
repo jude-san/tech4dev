997d43df49cf2ec412b20598d09ff5ac5582918b
Jude Nyamekye Koomson <61810349+jude-san@users.noreply.github.com>
2024-12-14 18:05:38 +0000

    Update variables.tf

--- Week2_assign_terraform/terraform/variables.tf
+++ Week2_assign_terraform/terraform/variables.tf
@@ -1,9 +1,9 @@
 variable "datadog_api_key" {
   description = "Datadog API Key"
-  default     = "9dc7523d32ea792a4cd75aa8cbee9d47"
+  default     = ""
 }
 
 variable "datadog_app_key" {
   description = "Datadog APP Key"
-  default     = "90f6c702d05a9de0aeebbd4b218a75d8eaef9d09"
+  default     = ""
 }
